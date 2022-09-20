70c0684078ddd43addc4caea1189372fc81ff4a3
Simonyan01 <starmafia01@mail.ru>
2022-09-20 14:54:25 +0400

    Success

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Lyov from './Lyov/Lyov';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+   <React.StrictMode>
+  <Lyov />
+   </React.StrictMode>
+);
+
+
